bcedafd5ba8cd4028e2f4ce5ffef67e9cb602b59
Sai Sanjay <97831658+sanjay7178@users.noreply.github.com>
2025-01-06 00:16:26 +0530

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -65,8 +65,9 @@
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
     };
-
+    package =  config.boot.kernelPackages.nvidiaPackages.stable; 
   };
+  services.xserver.displayManager.job.preStart = "sleep 10";
 
   # Configure keymap in X11
   services.xserver.xkb = {
